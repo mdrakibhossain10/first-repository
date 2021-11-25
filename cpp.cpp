d2f4c891a7de1970674db4ad249a1598a20b5e8b
mdrakibhossain10 <rakibb165@gmail.com>
2021-11-25 16:51:47 +0600

    cpp code modified

--- cpp.cpp
+++ cpp.cpp
@@ -1,10 +1,11 @@
-#include<bits/stdc++>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-  int a,b,c;
-  cin>>a>>b;
-  c=a+b;
-  cout<<c<<endl;
+  int a,b=0;
+  cin>>a;
+ for(int i=1;i<a;i++)
+    b+=i;
+cout<<b<<endl;
   return 0;
 }
